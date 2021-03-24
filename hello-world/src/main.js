f521200cb7a8d5c7d3fb3ef68221cfdb711c47dc
Benjamin Scown <1100015261@edu.cbe.ab.ca>
2021-03-24 13:31:13 -0600

    Added MapBox SDK

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -6,3 +6,11 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+/*var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGRldyIsImEiOiJja21udHhybWkwYng4Mm5vY3d3YnFtM2M1In0.rwhJ_tmCh3clW9urJaxQgw';
+var map = new mapboxgl.Map({
+container: 'YOUR_CONTAINER_ELEMENT_ID',
+style: 'mapbox://styles/mapbox/streets-v11'
+});*/
